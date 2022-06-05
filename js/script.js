686a70e4d46438a38787a42ecbb699c59da22acc
Bruno Mazzi <houille@yahoo.fr>
2022-06-06 00:52:52 +0200

    Add footer, minor fix

--- js/script.js
+++ js/script.js
@@ -16,18 +16,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     btn.addEventListener("click", function() {
-    if (document.body.classList.contains('light-mode')) {
-        document.body.classList.toggle("light-mode");
-        document.body.classList.toggle("dark-mode");
-        document.querySelector('.btn-toggle').innerHTML = 'Interface clair'
-        var theme = "dark";
-    } else {
-        document.body.classList.toggle("dark-mode");
-        document.body.classList.toggle("light-mode");
-        document.querySelector('.btn-toggle').innerHTML = 'Interface sombre'
-        var theme = "light";
-    }
+        if (document.body.classList.contains('light-mode')) {
+            document.body.classList.toggle("light-mode");
+            document.body.classList.toggle("dark-mode");
+            document.querySelector('.btn-toggle').innerHTML = 'Interface clair'
+            var theme = "dark";
+        } else {
+            document.body.classList.toggle("dark-mode");
+            document.body.classList.toggle("light-mode");
+            document.querySelector('.btn-toggle').innerHTML = 'Interface sombre'
+            var theme = "light";
+        }
 
-    localStorage.setItem("theme", theme);
+        localStorage.setItem("theme", theme);
     });
+
+    
 });
